f992c381a0d88aa98d2078746b63516b04dcd261
Serge <sergelab@gmail.com>
2022-07-08 01:12:05 +0300

    added: product card page, customized 404

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import abort, Flask, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from typing import Optional
 
@@ -14,9 +14,38 @@ app.config.from_mapping(
 DebugToolbarExtension(app)
 
 
+def get_products():
+	return [
+		{'id': 1, 'title': 'Товар 1', 'description': 'Описание 1'},
+		{'id': 2, 'title': 'Товар 2', 'description': 'Описание 2'},
+		{'id': 3, 'title': 'Товар 3', 'description': 'Описание 3'},
+		{'id': 4, 'title': 'Товар 4', 'description': 'Описание 4'},
+		{'id': 5, 'title': 'Товар 5', 'description': 'Описание 5ч'}
+	]
+
+
 @app.route('/')
 def index():
-	return render_template("index.j2")
+	_products = get_products()
+
+	return render_template("index.j2", products=_products)
+
+
+@app.route('/product/<int:product_id>')
+def product_card(product_id: int):
+	_products = get_products()
+	_product = None
+
+	for _p in _products:
+		if _p.get('id') == product_id:
+			_product = _p
+
+			break
+
+	if not _product:
+		abort(404)
+
+	return render_template("product_card.j2", product=_product)
 
 
 @app.route('/more')
@@ -56,3 +85,8 @@ def calc():
 		result = calculation(form.a.data, form.b.data, form.op.data)
 
 	return render_template("calc.j2", form=form, result=result)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.j2'), 404
