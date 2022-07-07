b88510dd1aa9b581c264e51a3503624e65b96e0f
Serge <sergelab@gmail.com>
2022-07-08 02:35:25 +0300

    login, logout, registration views added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
-from flask import abort, Flask, render_template, flash
+from flask import abort, current_app, Flask, redirect, url_for, request, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, UserMixin, login_required
 from typing import Optional
 from sqlalchemy import Column, Integer, Text
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from .forms import CalcForm
+
+from .forms import CalcForm, RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
@@ -13,10 +16,15 @@ app.config.from_mapping(
 	SECRET_KEY='dev111',
 	DEBUG_TB_PROFILER_ENABLED=True,
 	SQLALCHEMY_TRACK_MODIFICATIONS=False,
+	DEBUG_TB_INTERCEPT_REDIRECTS=False,
 	SQLALCHEMY_DATABASE_URI='postgresql://localhost:5432/miron_shop_test'
 )
 DebugToolbarExtension(app)
 db = SQLAlchemy(app)
+lm = LoginManager(app)
+
+
+lm.login_view = 'login'
 
 
 class Product(db.Model):
@@ -25,6 +33,34 @@ class Product(db.Model):
 	id = Column(Integer(), primary_key=True)
 	title = Column(Text(), nullable=False)
 	description = Column(Text())
+
+
+class User(db.Model, UserMixin):
+	__tablename__ = 'users'
+
+	id = Column(Integer(), primary_key=True)
+	pw_hash = Column(Text, nullable=False, default='<unset>')
+	username = Column(Text, nullable=False, unique=True)
+	name = Column(Text)
+
+	@property
+	def password(self):
+		return self.pw_hash
+
+	@password.setter
+	def password(self, value):
+		if value:
+			self.pw_hash = generate_password_hash(value)
+		else:
+			self.pw_hash = '<unset>'
+
+	def check_password(self, password):
+		return check_password_hash(self.pw_hash, password)
+
+
+@lm.user_loader
+def user_loader(user_id: int):
+	return User.query.filter(User.id == user_id).one_or_none()
 
 
 def get_products():
@@ -77,6 +113,7 @@ def calculation(a: int, b: int, op: int) -> Optional[int]:
 
 
 @app.route('/calculator', methods=['GET', 'POST'])
+@login_required
 def calc():
 	form = CalcForm()
 	result = None
@@ -90,3 +127,53 @@ def calc():
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.j2'), 404
+
+
+@app.route('/user/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm(**request.args)
+
+	if form.validate_on_submit():
+		_user = User.query.filter(User.username == form.username.data).one_or_none()
+		if not _user:
+			flash('Пользователь не найден', 'danger')
+			return redirect(url_for('login'))
+
+		if _user.check_password(form.password.data) and login_user(_user):
+			return redirect(form.next.data or url_for('index'))
+		else:
+			flash('Неверный пароль', 'danger')
+
+	return render_template('login.j2', form=form)
+
+
+@app.route('/user/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
+
+@app.route('/user/registration', methods=['GET', 'POST'])
+def registration():
+	form = RegistrationForm()
+
+	if form.validate_on_submit():
+		obj = User()
+		db.session.add(obj)
+		form.populate_obj(obj)
+
+		try:
+			db.session.commit()
+		except Exception as e:
+			current_app.logger.exception(e)
+			db.session.rollback()
+		else:
+			return redirect(url_for('index'))
+
+	return render_template("registration.j2", form=form)
+
+
+
+@app.route('/user/profile', methods=['GET', 'POSt'])
+def profile():
+	pass
